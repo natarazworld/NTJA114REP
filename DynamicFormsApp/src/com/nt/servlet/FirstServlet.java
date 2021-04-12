d29b663e38e8a0e4eb5b2740f07eb000d08bfaef
Nareshit <Nareshit@DESKTOP-IUDAAVL>
2021-04-12 12:24:45 +0530

    dynamic forms

---
+++ DynamicFormsApp/src/com/nt/servlet/FirstServlet.java
@@ -0,0 +1,60 @@
+package com.nt.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet("/firsturl")
+public class FirstServlet extends HttpServlet {
+       
+
+	public  void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		//get PrintWriter
+		PrintWriter pw=res.getWriter();
+		//set response content type
+		res.setContentType("text/html");
+		//read form1/req1  data
+		String pname=req.getParameter("pname");
+		String pfname=req.getParameter("pfname");
+		String paddrs=req.getParameter("paddrs");
+		String ms=req.getParameter("ms");
+		if(ms==null)
+			ms="single";
+		//Generate form2 dynamically based on the marital status
+		if(ms.equalsIgnoreCase("married")) {
+		      pw.println("<h1 style='color:red;text-align:center'>Submit Married life Details");
+		      pw.println("<form action='secondurl' method='Post'>");
+		       pw.println("<table  bgcolor='pink' align='center'> ");
+		       pw.println("<tr><td> spouse name::</td><td> <input type='text' name='f2t1'>  </td> </tr>");
+		       pw.println("<tr><td>No.of kids:: </td><td> <input type='text' name='f2t2'>  </td> </tr>");
+		       pw.println("<tr><td colspan='2'><input type='submit' value='submit'></td></tr>");
+		       pw.println("</table>");
+		      pw.println("</from>");
+		}
+		else {
+			  pw.println("<h1 style='color:red;text-align:center'>submit  Bachelor life Details");
+		      pw.println("<form action='secondurl' method='Post'>");
+		       pw.println("<table  bgcolor='grey' align='center'> ");
+		       pw.println("<tr><td> When do want to marry?::</td><td> <input type='text' name='f2t1'>  </td> </tr>");
+		       pw.println("<tr><td>Why do want to marry? </td><td> <input type='text' name='f2t2'>  </td> </tr>");
+		       pw.println("<tr><td colspan='2'><input type='submit' value='submit'></td></tr>");
+		       pw.println("</table>");
+		      pw.println("</from>");
+		}
+		//close stream
+		pw.close();
+	}
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		doGet(req, res);
+	}
+
+}
---
+++ DynamicFormsApp/src/com/nt/servlet/SecondServlet.java
@@ -0,0 +1,45 @@
+package com.nt.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/secondurl")
+public class SecondServlet extends HttpServlet {
+       
+
+	public  void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		//get PrintWriter
+		PrintWriter pw=res.getWriter();
+		//set response content type
+		res.setContentType("text/html");
+		//read form1/req1  data (will not come)
+		String pname=req.getParameter("pname");
+		String pfname=req.getParameter("pfname");
+		String paddrs=req.getParameter("paddrs");
+		String ms=req.getParameter("ms");
+		//read form2/req2 data
+		String f2val1=req.getParameter("f2t1");
+		String f2val2=req.getParameter("f2t2");
+		//generate dynamic web page displaying both form1/req1 and form2/req2 data
+		//Display form1/req1 data 
+		pw.println("<br><h1 style='color:red;text-align:center'>  Form1 / req1 data  "+pname+" ...."+pfname+"...."+paddrs+"....."+ms+"</h1>");
+		pw.println("<br><h1 style='color:red;text-align:center'>  Form2 / req2 data  "+f2val1+" ...."+f2val2+"</h1>");
+		
+		//add hyperlink
+		pw.println("<a href='form.html'>home </a>");
+		//close stream
+		pw.close();
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		doGet(req, res);
+	}
+
+}
